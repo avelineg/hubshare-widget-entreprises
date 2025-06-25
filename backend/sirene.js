8c292e64865d09ae21f1945239415053c179a1fc
avelineg <a.guenneugues@cmexpert.fr>
2025-06-25 17:57:26 +0200

    Update sirene.js

--- backend/sirene.js
+++ backend/sirene.js
@@ -1,20 +1,20 @@
-const axios = require(axios);
+const axios = require("axios");
 
 const SIRENE_API_URL = "https://api.sirene.fr/sirene/V3.11";
 
 async function searchSirene(query, apiKey) {
   let endpoint, params = {};
-  if (^d{14}$.test(query)) {
-    endpoint = `etablissement${query}`;
-  } else if (^d{9}$.test(query)) {
-    endpoint = `unitelegale${query}`;
+  if (/^\d{14}$/.test(query)) {
+    endpoint = `/etablissement/${query}`;
+  } else if (/^\d{9}$/.test(query)) {
+    endpoint = `/unitelegale/${query}`;
   } else {
-    endpoint = `siret`;
-    params.q = `denominationUniteLegale${query}`;
+    endpoint = `/siret`;
+    params.q = `denominationUniteLegale:${query}*`;
     params.nombre = 10;
   }
   const url = `${SIRENE_API_URL}${endpoint}`;
-  const headers = { Authorization apiKey };
+  const headers = { Authorization: apiKey };
   const { data } = await axios.get(url, { headers, params });
   return data;
 }
